6602d8db03ecfbc05f816ac8e9c1c89dfec530f6
akaniell <akaniell@yandex.ru>
2025-04-09 21:57:48 +0300

    Реализовано добавление врагов.

--- src/routes/battle/+page.svelte
+++ src/routes/battle/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import EnemyCreator from "$lib/components/EnemyCreator.svelte";
   import GameBoard from "$lib/components/GameBoard.svelte";
 </script>
 
 <div>
   Бой
   <GameBoard></GameBoard>
+  <EnemyCreator></EnemyCreator>
 </div>
